38bfa03da05f5ef0f0f697dc514f698436850c28
ZHEHG,YU-HENG <48355136+kenchick2S@users.noreply.github.com>
2022-08-20 10:43:12 +0800

    Update SVD.cpp

--- C++/SVD.cpp
+++ C++/SVD.cpp
@@ -1,111 +1,201 @@
-#include <Eigen/Core>
-#include <Eigen/SparseCore>
-#include <Eigen/SVD>
-#include <iostream>
-#include <random>  // Requires C++ 11
+#include<opencv2/opencv.hpp>
+#include<iostream>
 
-#include <Spectra/contrib/PartialSVDSolver.h>
+//g++ SVD.cpp -lopencv_core455 -lopencv_highgui455 -lopencv_imgproc455 -lopencv_imgcodecs455 -I D:\opencv\opencv\include -o SVD.exe
 
-using namespace Spectra;
 
-#include "catch.hpp"
-
-using Matrix = Eigen::MatrixXd;
-using Vector = Eigen::VectorXd;
-using SpMatrix = Eigen::SparseMatrix<double>;
-
-// Generate random sparse matrix
-SpMatrix gen_sparse_data(int m, int n, double prob = 0.5)
-{
-    SpMatrix mat(m, n);
-    std::default_random_engine gen;
-    gen.seed(0);
-    std::uniform_real_distribution<double> distr(0.0, 1.0);
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (distr(gen) < prob)
-                mat.insert(i, j) = distr(gen) - 0.5;
+void maxloc(cv::Mat A , float *max, cv::Point *max_loc){
+    double temp = 0.0;
+    for(int i=0; i<A.rows; i++){
+        for(int j=0; j<A.cols; j++){
+            if(i != j){
+                if(std::abs(A.ptr<float>(i)[j]) > std::abs(temp)){
+                    temp = A.ptr<float>(i)[j];
+                    (*max_loc).x = i;
+                    (*max_loc).y = j;
+                }
+            }
         }
     }
-    return mat;
+    *max = temp;
 }
 
-template <typename MatType>
-void run_test(const MatType& mat, int k, int m)
-{
-    PartialSVDSolver<MatType> svds(mat, k, m);
-    int nconv = svds.compute();
+void SVD(cv::Mat A, cv::Mat U, cv::Mat S, cv::Mat Vt){
+        int m = A.rows, n = A.cols, temp;
+        //std::cout << m << "," << n << std::endl;
+        if(m < n){
+            temp = m;
+            m = n;
+            n = temp;
+        }
+        else{
+            //cv::transpose(A, A);
+        }
+        U = cv::Mat::zeros(cv::Size(m, m), CV_32FC1);
+        S = cv::Mat::zeros(cv::Size(n, 1), CV_32FC1);
+        Vt = cv::Mat::zeros(cv::Size(n, n), CV_32FC1);
 
-    INFO("nconv = " << nconv);
-    REQUIRE(nconv == k);
+        cv::Mat A_t = cv::Mat::zeros(cv::Size(m, m), CV_32FC1);
 
-    Vector svals = svds.singular_values();
-    Matrix U = svds.matrix_U(k);
-    Matrix V = svds.matrix_V(k);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                A_t.ptr<float>(i)[j] =  A.ptr<float>(i)[j];
+            }
+        }
+        // std::cout<< A_t.rowRange(m-2, m) << std::endl;
 
-    // SVD solver from Eigen
-    // Requires dense matrices
-    Matrix mat_dense = Matrix(mat);
-    Eigen::JacobiSVD<Matrix> svd(mat, Eigen::ComputeThinU | Eigen::ComputeThinV);
-    Vector svals_eigen = svd.singularValues();
-    Matrix U_eigen = svd.matrixU();
-    Matrix V_eigen = svd.matrixV();
+        float max;
+        cv::Point max_loc;
+        float theta, cos, sin;
+        cv::Mat A_new = cv::Mat::zeros(cv::Size(m, m), CV_32FC1);
+        // std::cout<< A_t.ptr<float>(max_loc.x)[max_loc.y] <<", "<< max <<std::endl;
+        for(int i=0; i<std::max(m, 30); i++){
+            maxloc(A_t , &max, &max_loc);
+            //std::cout<<"max: "<< max <<std::endl;
+            //std::cout<<"max: "<< max_loc <<std::endl;
+            theta = std::atan2(2*A_t.ptr<float>(max_loc.x)[max_loc.y], A_t.ptr<float>(max_loc.x)[max_loc.x]-A_t.ptr<float>(max_loc.y)[max_loc.y])*0.5;
+            cos = std::cos(theta);
+            sin = std::sin(theta);
+            //std::cout<<"theta: "<< theta <<std::endl;
+            //std::cout<<"sin: "<< sin <<std::endl;
+            A_t.copyTo(A_new);
 
-    double err = (svals - svals_eigen.head(k)).array().abs().maxCoeff();
-    INFO("Residual of singular values = " << err);
-    REQUIRE(err == Approx(0.0).margin(1e-9));
+            for(int j=0; j<m; j++){
+                 A_new.ptr<float>(j)[max_loc.x] = cos*A_t.ptr<float>(j)[max_loc.x] + sin*A_t.ptr<float>(j)[max_loc.y];
+            }
+            for(int j=0; j<m; j++){
+                 A_new.ptr<float>(j)[max_loc.y] = cos*A_t.ptr<float>(j)[max_loc.y] - sin*A_t.ptr<float>(j)[max_loc.x];
+            }
+            //std::cout<< A_new <<std::endl;
+            //std::cout<<"A_t: "<< A_t <<std::endl;
+            A_new.ptr<float>(max_loc.x)[max_loc.x] = cos*A_new.ptr<float>(max_loc.x)[max_loc.x] + sin*A_new.ptr<float>(max_loc.y)[max_loc.x];
+            A_new.ptr<float>(max_loc.y)[max_loc.y] = cos*A_new.ptr<float>(max_loc.y)[max_loc.y] - sin*A_new.ptr<float>(max_loc.x)[max_loc.y];
+            A_new.ptr<float>(max_loc.x)[max_loc.y] = 0;
+            A_new.ptr<float>(max_loc.y)[max_loc.x] = 0;
+            
 
-    err = (U.array().abs() - U_eigen.leftCols(k).array().abs()).abs().maxCoeff();
-    INFO("Residual of left singular vectors = " << err);
-    REQUIRE(err == Approx(0.0).margin(1e-9));
+            for(int j=0; j<m; j++){
+                for(int k=0; k<m; k++){
+                    A_new.ptr<float>(j)[k] = A_new.ptr<float>(k)[j];
+                }
+            }
+            /*for(int k=0; k<m; k++){
+                    A_new.ptr<float>(max_loc.y)[k] = A_new.ptr<float>(k)[max_loc.y];
+            }*/
+            A_new.copyTo(A_t);
+            //std::cout<< A_t <<std::endl;
+        }
+        for(int i=0; i<m; i++){
+            std::cout<<A_t.ptr<float>(i)[i]<<" ";
+        }
+        std::cout<<std::endl;
+}
+void SVD_1(cv::Mat A, cv::Mat U, cv::Mat S, cv::Mat Vt){
+        int m = A.rows, n = A.cols, temp;
+        //std::cout << m << "," << n << std::endl;
+        if(m < n){
+            temp = m;
+            m = n;
+            n = temp;
+        }
+        else{
+            //cv::transpose(A, A);
+        }
+        U = cv::Mat::zeros(cv::Size(m, m), CV_32FC1);
+        S = cv::Mat::zeros(cv::Size(n, 1), CV_32FC1);
+        Vt = cv::Mat::zeros(cv::Size(n, n), CV_32FC1);
 
-    err = (V.array().abs() - V_eigen.leftCols(k).array().abs()).abs().maxCoeff();
-    INFO("Residual of right singular vectors = " << err);
-    REQUIRE(err == Approx(0.0).margin(1e-9));
+        cv::Mat A_t = cv::Mat::zeros(cv::Size(m, m), CV_32FC1);
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                A_t.ptr<float>(i)[j] =  A.ptr<float>(i)[j];
+            }
+        }
+        // std::cout<< A_t.rowRange(m-2, m) << std::endl;
+
+        float max;
+        cv::Point max_loc;
+        float theta, cos, sin;
+        cv::Mat A_new = cv::Mat::zeros(cv::Size(m, m), CV_32FC1);
+        // std::cout<< A_t.ptr<float>(max_loc.x)[max_loc.y] <<", "<< max <<std::endl;
+        for(int i=0; i<std::max(m, 30); i++){
+            maxloc(A_t , &max, &max_loc);
+            //std::cout<<"max: "<< max <<std::endl;
+            //std::cout<<"max: "<< max_loc <<std::endl;
+            theta = std::atan2(2*A_t.ptr<float>(max_loc.x)[max_loc.y], A_t.ptr<float>(max_loc.x)[max_loc.x]-A_t.ptr<float>(max_loc.y)[max_loc.y])*0.5;
+            cos = std::cos(theta);
+            sin = std::sin(theta);
+            //std::cout<<"theta: "<< theta <<std::endl;
+            //std::cout<<"sin: "<< sin <<std::endl;
+            A_t.copyTo(A_new);
+
+            for(int j=0; j<m; j++){
+                 A_new.ptr<float>(j)[max_loc.x] = cos*A_t.ptr<float>(j)[max_loc.x] + sin*A_t.ptr<float>(j)[max_loc.y];
+            }
+            for(int j=0; j<m; j++){
+                 A_new.ptr<float>(j)[max_loc.y] = cos*A_t.ptr<float>(j)[max_loc.y] - sin*A_t.ptr<float>(j)[max_loc.x];
+            }
+            //std::cout<< A_new <<std::endl;
+            //std::cout<<"A_t: "<< A_t <<std::endl;
+            A_new.ptr<float>(max_loc.x)[max_loc.x] = cos*A_new.ptr<float>(max_loc.x)[max_loc.x] + sin*A_new.ptr<float>(max_loc.y)[max_loc.x];
+            A_new.ptr<float>(max_loc.y)[max_loc.y] = cos*A_new.ptr<float>(max_loc.y)[max_loc.y] - sin*A_new.ptr<float>(max_loc.x)[max_loc.y];
+            A_new.ptr<float>(max_loc.x)[max_loc.y] = 0;
+            A_new.ptr<float>(max_loc.y)[max_loc.x] = 0;
+            
+
+            for(int j=0; j<m; j++){
+                for(int k=0; k<m; k++){
+                    A_new.ptr<float>(j)[k] = A_new.ptr<float>(k)[j];
+                }
+            }
+            /*for(int k=0; k<m; k++){
+                    A_new.ptr<float>(max_loc.y)[k] = A_new.ptr<float>(k)[max_loc.y];
+            }*/
+            A_new.copyTo(A_t);
+            //std::cout<< A_t <<std::endl;
+        }
+        for(int i=0; i<m; i++){
+            std::cout<<A_t.ptr<float>(i)[i]<<" ";
+        }
+        std::cout<<std::endl;
 }
 
-TEST_CASE("Partial SVD of tall dense matrix [1000x100]", "[svds_dense_tall]")
-{
-    std::srand(123);
+int main(int argc, char** argv) {
 
-    const Matrix A = Matrix::Random(1000, 100);
-    int k = 5;
-    int m = 10;
+    cv::Mat src = cv::imread("D:/image/python/lena.jpg", 0);
 
-    run_test<Matrix>(A, k, m);
-}
+    cv::Mat result;
+    cv::Mat tempt;
+    cv::Mat U, S, V;
+    cv::Mat U1, S1, V1;
+    src.convertTo(tempt, CV_32FC1);
+    float mat[3][3] =  {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+    cv::Mat temp = cv::Mat::zeros(cv::Size(3, 3), CV_32FC1);
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++)
+            temp.ptr<float>(i)[j] = mat[i][j];
+    }
+    SVD(temp, U1, S1, V1);
+    //SVD(tempt, U, S, V);
+    cv::SVD::compute(tempt, S, U, V);//SVD分解
 
-TEST_CASE("Partial SVD of wide dense matrix [1000x100]", "[svds_dense_wide]")
-{
-    std::srand(123);
+    cv::Mat s = cv::Mat::zeros(cv::Size(S.rows, S.rows), CV_32FC1);
+    double theratio = 0.1;
+    int len = theratio * S.rows;
+    for (int i = 0; i < len; ++i) s.ptr<float>(i)[i] = S.ptr<float>(i)[0];
 
-    const Matrix A = Matrix::Random(100, 1000);
-    int k = 5;
-    int m = 10;
+    //std::cout << S.cols; // N rows / 1 cols (Nx1)
 
-    run_test<Matrix>(A, k, m);
-}
+    result = U * s * V; 
 
-TEST_CASE("Partial SVD of tall sparse matrix [1000x100]", "[svds_sparse_tall]")
-{
-    std::srand(123);
+    std::cout<< S << std::endl;
+    cv::Mat outputImg;  
+    result.convertTo(outputImg, CV_8UC1);
 
-    const SpMatrix A = gen_sparse_data(1000, 100, 0.1);
-    int k = 5;
-    int m = 10;
+    cv::imshow("origin", src);
+    cv::imshow("compressed", outputImg);
 
-    run_test<SpMatrix>(A, k, m);
-}
-
-TEST_CASE("Partial SVD of wide sparse matrix [1000x100]", "[svds_sparse_wide]")
-{
-    std::srand(123);
-
-    const SpMatrix A = gen_sparse_data(100, 1000, 0.1);
-    int k = 5;
-    int m = 10;
-
-    run_test<SpMatrix>(A, k, m);
+    cv::waitKey(0);
+    return 0;
 }
